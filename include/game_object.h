f14075a02605195251c815ec304db845703552f1
Arcarox <arcarox@localhost.localdomain>
2020-01-02 23:48:53 +0100

    [ADD] | start of a doxygen for my_game lib

--- include/game_object.h
+++ include/game_object.h
@@ -59,13 +59,68 @@ typedef struct game_object {
     struct game_object *next;
 } game_object_t;
 
-void update_game_object_state(struct game_object *, int);
-void init_game_object_frame(struct game_object *);
-struct game_object *create_game_object(struct game_object *, char *, \
-sfVector2f, object_type);
-void init_game_object(game_object_t *);
-void update_game_object_frame(struct game_object *);
-void update_game_object_frame_reversed(struct game_object *);
+////////////////////////////////////////////////////////////
+/// \brief Initialize game_object frame
+///
+/// Initialize game_object frame to the first frame of the frames keys of his actual state
+///
+/// \param game_object_t object to initialize
+////////////////////////////////////////////////////////////
+void init_game_object_frame(struct game_object *object);
+
+////////////////////////////////////////////////////////////
+/// \brief Update game_object frame
+///
+/// Update game_object frame to the next frame in the frames keys of his actual 
+/// state and his is already at the last depending on the value of the loop bool it will start from start or won't change the actual frame
+///
+/// \param game_object_t object to update
+////////////////////////////////////////////////////////////
+void update_game_object_frame(struct game_object *object);
+
+////////////////////////////////////////////////////////////
+/// \brief Update game_object frame
+///
+/// Update game_object frame to the previous frame in the frames keys of his actual
+/// state and his is already at the first depending on the value of the loop bool it will stop
+///
+/// \param game_object_t object to update
+////////////////////////////////////////////////////////////
+void update_game_object_frame_reversed(struct game_object *object);
+
+////////////////////////////////////////////////////////////
+/// \brief Update game_object state
+///
+/// Change game_object state and update his frame keys
+///
+/// \param game_object_t object to update
+/// \param int state the new state of the object
+////////////////////////////////////////////////////////////
+void update_game_object_state(struct game_object *object , int state);
+
+////////////////////////////////////////////////////////////
+/// \brief Create a game_object
+///
+/// Create and initialize a gameobject with a sprite, a texture, a position and a type.
+///
+/// \param game_object_t last_object of the linked list
+/// \param string path of the texture of the object
+/// \param vector pos of the sprite
+/// \param object_type type of the game_object
+///
+/// \return game_object_t *object
+////////////////////////////////////////////////////////////
+struct game_object *create_game_object(struct game_object *last, char *path, \
+sfVector2f pos, object_type type);
+
+////////////////////////////////////////////////////////////
+/// \brief Initialize a game_object
+///
+/// Simply make pointeur equal to NULL and initialize value to zero
+///
+/// \param game_object_t object to initialize
+////////////////////////////////////////////////////////////
+void init_game_object(game_object_t *object);
 void move_object(game_object_t *);
 void draw_objects(game_object_t *, sfRenderWindow *, int);
 void update_objects(scene_t *, game_object_t *, game_t *);
